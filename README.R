1910c19bfd9c95a8e31ea75b338d3a09575d7cff
jetanley <jetanley@ncsu.edu>
2022-06-08 19:01:48 -0400

    still looking for data

--- README.R
+++ README.R
@@ -1,4 +1,6 @@
-# Interacting with APIs
+# Outputing code
 
 
-
+rmarkdown::render("README.Rmd", 
+                  output_format = "github_document", 
+                  output_options = list(html_preview = FALSE))
